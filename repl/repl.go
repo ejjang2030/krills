c7c3a4fc5545a2db4d7cbdf41ad0da572f0699b2
ejjang2030 <ejjang2030@gmail.com>
2022-11-26 16:16:40 +0900

    error handling and environment for binding

--- repl/repl.go
+++ repl/repl.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"krills/evaluator"
 	"krills/lexer"
+	"krills/object"
 	"krills/parser"
 )
 
@@ -23,6 +24,7 @@ const KRILLS_SKIN = `||    //  ||======     ||   ||          ||          /======
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Fprintf(out, PROMPT)
@@ -41,7 +43,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
